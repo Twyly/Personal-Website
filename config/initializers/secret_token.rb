5b224ca8ae6e81158483990b4e122ddb067cd4e5
Teddy Wyly <wylynout@gmail.com>
2014-01-24 20:58:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teddy::Application.config.secret_key_base = '3015b560499dd663e9dcd90b9f02ce3bab15888ea3769a720ce27626d72fff5f46b5f525be316ac16842e9b6ed08f27fdf8a692233f5532578380d6d71e815ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Teddy::Application.config.session_store :cookie_store, key: '_Teddy_session'
